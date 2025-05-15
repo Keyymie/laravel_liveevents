5e263aacfcfc7712350d5aacfcfb3b179e874250
Keyymie <35221852+Keyymie@users.noreply.github.com>
2025-05-15 16:47:40 +0200

    Update app2.blade.php

--- resources/views/layouts/app2.blade.php
+++ resources/views/layouts/app2.blade.php
@@ -6,10 +6,7 @@
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body>
-    {{-- Inclut ta navbar --}}
     @include('navbar.navbar')
-
-    {{-- Contenu de la page --}}
     <main>
         @yield('content')
     </main>
